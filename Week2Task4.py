e586b2513edb18a74748e6577df847e89d5670b0
Muhammad Nauman <33566385+creed360@users.noreply.github.com>
2019-06-20 17:58:54 +0500

    Comment added.

--- Week2Task4.py
+++ Week2Task4.py
@@ -1,3 +1,13 @@
+"""Define a class which has at least two methods:
+
+getString: to get a string from console input
+printString: to print the string in upper case.
+
+Also please include simple test function to test the class methods.
+
+Hints:
+Use __init__ method to construct some parameters
+"""
 print("-----------------WEEK 2 Task 4-----------------")
 
 class Wee2Task4:
