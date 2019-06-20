dcc105501144d89a3d6e28ef9b80bfbd5734595f
Muhammad Nauman <33566385+creed360@users.noreply.github.com>
2019-06-20 18:01:34 +0500

    Comment added.

--- Week2Task2.py
+++ Week2Task2.py
@@ -1,3 +1,7 @@
+"""Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 1000 and 2000 (both included).
+
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+"""
 print("-----------------WEEK 2 Task 2-----------------")
 numbers=[]
 for i in range(1000,2001):
